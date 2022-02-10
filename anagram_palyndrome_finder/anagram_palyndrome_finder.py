3665703bd1dee9f3e881a93f5dcbe3e371c844d2
AmyRidings <aaridings@gmail.com>
2022-02-10 13:27:29 -0700

    Update anagram_palyndrome_finder.py

--- anagram_palyndrome_finder/anagram_palyndrome_finder.py
+++ anagram_palyndrome_finder/anagram_palyndrome_finder.py
@@ -11,20 +11,40 @@ import argparse
 import anpatools
 
 def access_webpage(url):
-    '''Open and read URL'''
+    '''Opens URL and reads webpage'''
     webpage = urllib.request.urlopen(url)
     content = webpage.read().decode()
     return content
 
 def parse_page(content):
-    '''Takes raw html as input and parses text from <p> tags.
-    Returns a list of parsed text'''
+    """
+    Parameters
+    ----------
+    content : str
+        Raw HTML text.
+
+    Returns
+    -------
+    parsed_data : list
+        A list of strings parsed from <p> tags in raw HTML text.
+    """
     pars = anpatools.ParserHTML()
     pars.feed(str(content))
     parsed_data = pars.data_list
     return parsed_data
 
 def clean_data(data_list):
+    """
+    Parameters
+    ----------
+    data_list : list
+        A list of strings, each element may have one or more words.
+
+    Returns
+    -------
+    string_list : list
+        A new list of strings, each element being a single word.
+    """
     string_list = []
     for items in data_list:
         words = items.split(' ')
@@ -33,18 +53,19 @@ def clean_data(data_list):
     return string_list
 
 
-'''Add command line argument'''
+#Add command line argument
 parser = argparse.ArgumentParser(description='Process web url.')
 parser.add_argument('--url', type=str, required=True,\
                     help='Web address to the page to analyze.')
-'''Assign command line arguments'''
+
+#Assign command line arguments'
 args = parser.parse_args()
 URL = args.url
-'''Function calls'''
+
+#Function calls
 web_content = access_webpage(URL)
 parsed_list = parse_page(web_content)
 data_strings = clean_data(parsed_list)
-
 
 anagrams = [anpatools.Anagram(n) for n in data_strings]
 for anagram in anagrams:
